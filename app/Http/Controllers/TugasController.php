1bc3ca6d261ad2dbcb4e972bb4bee33b421893f2
maulanahafez <maulanahafez00@gmail.com>
2023-07-03 16:06:50 +0700

    tugas crud

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -14,4 +14,51 @@ class TugasController extends Controller
         ];
         return view('tugas.index', $data);
     }
+
+    public function create()
+    {
+        return view('tugas.create');
+    }
+
+    public function store(Request $req)
+    {
+        $validated = $req->validate([
+            'name' => ['required'],
+        ]);
+
+        $newProject = Tugas::create([
+            'name' => $req->name,
+        ]);
+
+        return redirect()->route('tugas.index')->with('successStore', 'New tugas has been created successfully');
+    }
+
+    public function show(Tugas $tugas)
+    {
+        $data = [
+            'tugas' => $tugas
+        ];
+
+        return view('tugas.detail', $data);
+    }
+
+    public function update(Tugas $tugas, Request $req)
+    {
+        $validated = $req->validate([
+            'name' => ['required'],
+        ]);
+
+        $updatedProject = $tugas->update([
+            'name' => $req->name,
+        ]);
+
+        return redirect()->route('tugas.show', ['tugas' => $tugas->id])->with('successUpdate', 'Project has been updated successfully');
+    }
+
+    public function destroy(Tugas $tugas)
+    {
+        $tugas->delete();
+
+        return redirect()->route('tugas.index')->with('successDestroy', 'Project has been deleted successfully');
+    }
 }
